7265ac6c5a7d05ee94689edb0c6afbf034e2653c
Råvên <raven.in.westfall@gmail.com>
2020-05-30 22:47:08 -0400

    Adding roll commands

--- MSgtOS.lua
+++ MSgtOS.lua
@@ -1,8 +1,5 @@
 local MSgtOS, addon_variables = ...
 
--- local myRoll = random(100)
--- print("Your rolle is " .. myRoll)
-
 local rarity_names = { 'Poor', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legendary' }
 local Dialog = LibStub("LibDialog-1.0")
 local rarity_threashold = 5
@@ -20,6 +17,7 @@ local message_color = {
 local non_guild_pug_threashold_limit = 1
 local current_loot = {}
 local group = 'Unknown'
+local player_name = GetUnitName('player')
 
 local copy_dialog = Dialog:Register("MSgtOSCopyDialog", {
 	text = "Loot Copy\nUse your copy keyboard short cut to copy loot tbale to clipboard",
@@ -123,20 +121,51 @@ local process_raid_message = function(message)
 	end
 end
 
+
+local make_roll = function(main_spec)
+	local my_roll = random(100)
+	local spec_type = 'OS'
+	if main_spec then spec_type = 'MS' end
+	roll_message = player_name .." rolls ".. my_roll .." (1-100) ".. spec_type
+	C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", roll_message, "RAID");
+end
+
+SLASH_MSGTOSOSROLL1 = '/osroll'
+SlashCmdList.MSGTOSOSROLL = function(msg, editbox)
+	make_roll(false)
+end
+
+SLASH_MSGTOSMSROLL1 = '/msroll'
+SlashCmdList.MSGTOSMSROLL = function(msg, editbox)
+	make_roll(true)
+end
+
+SLASH_MSGTOSPASSROLL1 = '/pass'
+SlashCmdList.MSGTOSPASSROLL = function(msg, editbox)
+	C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", player_name .." passes", "RAID");
+end
+
 local f = CreateFrame("Frame")
 f:RegisterEvent("LOOT_OPENED")
 f:RegisterEvent("RAID_ROSTER_UPDATE")
 f:RegisterEvent("GROUP_ROSTER_UPDATE")
 f:RegisterEvent("CHAT_MSG_RAID_WARNING");
+f:RegisterEvent("CHAT_MSG_ADDON")
 f:SetScript("OnEvent", function(self, event, ...)
 	if event == "LOOT_OPENED" and enable_logging then process_loot()
 	elseif event == "GROUP_ROSTER_UPDATE" or event == "RAID_ROSTER_UPDATE" then process_raid_change()
 	elseif event == "CHAT_MSG_RAID_WARNING" then
 		message = ...
 		process_raid_message(message)
+	elseif event == "CHAT_MSG_ADDON" then
+		message_type, addon_msg, level = ...
+                if message_type == 'MSgtOS_ROLL' then
+			DEFAULT_CHAT_FRAME:AddMessage(addon_msg, 1, 1, 0)
+		end
 	end
 end)
 
 
 -- In the case of a reload we want to process_raid_change
 process_raid_change()
+C_ChatInfo.RegisterAddonMessagePrefix("MSgtOS_ROLL")
