ec505a467fd13da4d9bbd432f4bb33c744b318fc
Råvên <raven.in.westfall@gmail.com>
2020-06-02 12:41:25 -0400

    [WIP] initial split of Roll and main file

--- MSOSRoll.lua
+++ MSOSRoll.lua
@@ -1,7 +1,6 @@
 -- TODO:
---	Fiz was offline when he got ML, make the addon check for ML when its started
---		This should work, needs testing
---	Get MS/OS wrking
+--	Get MS/OS sort working
+--	There is a 250 char limit in SendAddOnMessage.... need to get around that.
 
 local MSgtOS, addon_variables = ...
 
@@ -9,191 +8,346 @@ local rarity_names = { 'Poor', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legendary'
 local Dialog = LibStub("LibDialog-1.0")
 local player_name = GetUnitName('player')
 local current_roll_tracker = nil
+local current_roll_text = nil
 local legacy_rolls = {}
 local roll_opened = false
-local message_color = {
-        ['r'] = 0.45,
-        ['g'] = 0.0,
-        ['b'] = 1.0,
-}
+local master_looter = true
 
-local roll_tracker_dialog = Dialog:Register("MSgtOSRollTracker", {
-	text = "Loot Tracker",
-	width = 300,
-	on_show = function(self, data)
-		display_sorted_roll_tracker(self, current_roll_tracker)
-	end,
-	buttons = {
-		{ text = "End Roll", },
-		{ text = CLOSE, },
-	},
-	show_while_dead = true,
-	hide_on_escape = true,
-})
-
-local start_roll = function()
-	if current_roll_tracer ~= nil then
-		table.insert(legacy_rolls, current_roll_tracer)
-	end
-	current_roll_tracker = {
-		['item_name'] = item_name,
-		['passes'] = {},
-		['MS'] = {},
-		['OS'] = {},
-	}
-	roll_opened = true
+local send_chat_message = function(text)
+	DEFAULT_CHAT_FRAME:AddMessage("[MS > OS] ".. text, 0.45, 0.0, 1.0)
 end
 
-local make_roll = function(main_spec)
-	local my_roll = random(100)
-	local spec_type = 'OS'
-	if main_spec then spec_type = 'MS' end
-	roll_message = player_name .." rolls ".. my_roll .." (1-100) ".. spec_type
-	C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", roll_message, "RAID");
-end
-
-SLASH_MSGTOSOSROLL1 = '/osroll'
-SlashCmdList.MSGTOSOSROLL = function(msg, editbox)
-	if roll_opened then
-		make_roll(false)
-	else
-		print_no_roll_message()
-	end
-end
-
-SLASH_MSGTOSMSROLL1 = '/msroll'
-SlashCmdList.MSGTOSMSROLL = function(msg, editbox)
-	if roll_opened then
-		make_roll(true)
-	else
-		print_no_roll_message()
-	end
-end
-
-SLASH_MSGTOSPASSROLL1 = '/pass'
-SlashCmdList.MSGTOSPASSROLL = function(msg, editbox)
-	if roll_opened then
-		C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", player_name .." passes", "RAID");
-	else
-		print_no_roll_message()
-	end
-end
-
-print_no_roll_message = function()
-	DEFAULT_CHAT_FRAME:AddMessage("[MS > OS] There is not currently a roll opened", message_color["r"], message_color["g"], message_color["b"])
-end
-
-process_incoming_roll = function(addon_msg)
-	DEFAULT_CHAT_FRAME:AddMessage(addon_msg, 1, 1, 0)
-	if current_roll_tracker ~= nil then
-		-- Split this message and put it in our roll tracker
-		local message_segments = {}
-		for i in string.gmatch(addon_msg, "%S+") do
-			table.insert(message_segments, i)
-		end
-		local roll_type = nil
-		local roll_value = nil
-		if message_segments[2] == 'passes' then
-			table.insert(current_roll_tracker['passes'], message_segments[1])
-		else
-			roll_type = message_segments[5]
-			roll_value = message_segments[3]
-			if current_roll_tracker[roll_type][roll_value] == nil then
-				current_roll_tracker[roll_type][roll_value] = {}
-			end
-			table.insert(current_roll_tracker[roll_type][roll_value], message_segments[1])
-		end
-		display_sorted_roll_tracker()
-	end
-end
-
-local sort_roll_tracker_field = function(tbl)
+local get_sorted_keys = function(table_to_sort)
 	local keys = {}
-	for key in pairs(tbl) do
+	for key in pairs(table_to_sort) do
 		table.insert(keys, key)
 	end
 
-	table.sort(keys, function(a, b)
-		return tbl[a] < tbl[b]
-	end)
-
+	table.sort(keys, function(a, b) return a > b end)
 	return keys
 end
 
-display_sorted_roll_tracker = function(dialog, roll_to_display)
+local generate_roll_text = function()
 	--print('RAID_CLASS_COLORS')
 	--for key, value in pairs(RAID_CLASS_COLORS) do
 	--	print(key)
 	--end
 
 	-- First load everyone in the raid
-	local new_text = 'Loot Tracker\n'
-        local raid_classes = {}
-        for i = 1, MAX_RAID_MEMBERS do
-		user_name, _, _, _, user_class, _, _, _, _, _, _ = GetRaidRosterInfo(i);
-		if user_name ~= nil then
-			raid_classes[ user_name ] = user_class
-        	end
-	end
+	local new_text = '|cffffff00Loot Rolls\nFor |r'.. current_roll_tracker['item_link'] .."\n\n"
+	local raid_classes = {}
+--	for i = 1, MAX_RAID_MEMBERS do
+--		user_name, _, _, _, user_class, _, _, _, _, _, _ = GetRaidRosterInfo(i);
+--		if user_name ~= nil then
+--			raid_classes[ user_name ] = user_class
+--		end
+--	end
+
+	raid_classes['u1'] = true
+	raid_classes['u2'] = true
+	raid_classes['u3'] = true
+	raid_classes['u4'] = true
+	raid_classes['u5'] = true
+	raid_classes['u6'] = true
+	raid_classes['u7'] = true
+	raid_classes['u8'] = true
+	raid_classes['u9'] = true
+	raid_classes['u10'] = true
+	raid_classes['u11'] = true
+	raid_classes['u12'] = true
+	raid_classes['u13'] = true
+	raid_classes['u14'] = true
+	raid_classes['u15'] = true
+	raid_classes['u16'] = true
+	raid_classes['u17'] = true
+	raid_classes['u18'] = true
+	raid_classes['u19'] = true
+	raid_classes['u20'] = true
+	raid_classes['u21'] = true
+	raid_classes['u22'] = true
+	raid_classes['u23'] = true
+	raid_classes['u24'] = true
+	raid_classes['u25'] = true
+	raid_classes['u26'] = true
+	raid_classes['u27'] = true
+	raid_classes['u28'] = true
+	raid_classes['u29'] = true
+	raid_classes['u30'] = true
+	raid_classes['u31'] = true
+	raid_classes['u32'] = true
+	raid_classes['u33'] = true
+	raid_classes['u34'] = true
+	raid_classes['u35'] = true
+	raid_classes['u36'] = true
+	raid_classes['u37'] = true
+	raid_classes['u38'] = true
+	raid_classes['Chegg'] = true
+	raid_classes['Anabella'] = true
 
 
 	-- Extract out the main spec rolls
-	new_text = new_text .."Main Spec Rolls\n"
-	if current_roll_tracker['MS'] ~= nil then
-		local sorted_keys = sort_roll_tracker_field(current_roll_tracker['MS'])
-		for _, key in ipairs(sorted_keys) do
-			print(current_roll_tracker['MS'][key][1])
-			print(current_roll_tracker['MS'][key][2])
-			new_text = new_text .. value[2] .."  ".. value[1] .."\n"
+	new_text = new_text .."|cffffff00Main Spec Rolls|r\n"
+	local sorted_keys = get_sorted_keys(current_roll_tracker['MS'])
+	local has_roll = false
+	for _, key in ipairs(sorted_keys) do
+		for _, user in pairs(current_roll_tracker['MS'][key]) do
+			new_text = new_text .. key .." ".. user .."\n"
+			raid_classes[user] = nil
+			has_roll = true
 		end
-	else
-		new_text = new_text .."None\n"
 	end
+	if not has_roll then new_text = new_text .."|cff666666None|r\n" end
 
 
-	print("TESTING")
---	test_dir = {
---		{'Raven', '12'},
---		{'Chegg', '98'},
---	}
---	table.sort(test_dir, sort_roll_tracker_field)
---	for item, value in pairs(test_dir) do
---		print(value[1])
---		print(value[2])
---	end
---	print("END_TESTING")
---
---	-- Extract out the off spec rolls
---	new_text = new_text .."Off Spec Rolls\n"
---	if current_roll_tracker['OS'] ~= nil then
---		table.sort(current_roll_tracker['OS'], sort_roll_tracker_field)
---		for item, value in pairs(current_roll_tracker['OS']) do
---			new_text = new_text .. value[2] .."  ".. value[1] .."\n"
---		end
---	else
---		new_text = new_text .."None\n"
---	end
---
---
---	-- Extract out the pass rolles
---	new_text = new_text .."Passed\n"
---	if current_roll_tracker['passes'] ~= nil then
---		table.sort(current_roll_tracker['passes'], sort_roll_tracker_field)
---		for item, value in pairs(current_roll_tracker['passes']) do
---			for key,value in pairs(value) do
---				new_text = new_text .. value .."\n"
---			end
---		end
---	else
---		new_text = new_text .."None\n"
---	end
+	-- Extract out the off spec rolls
+	new_text = new_text .."\n|cffffff00Off Spec Rolls|r\n"
+	local sorted_keys = get_sorted_keys(current_roll_tracker['OS'])
+	has_roll = false
+	for _, key in ipairs(sorted_keys) do
+		for _, user in pairs(current_roll_tracker['OS'][key]) do
+			new_text = new_text .. key .." ".. user .."\n"
+			raid_classes[user] = nil
+			has_roll = true
+		end
+	end
+	if not has_roll then new_text = new_text .."|cff666666None|r\n" end
+
+
+	-- Extract out the pass rolles
+	new_text = new_text .."\n|cffffff00Passed|r\n"
+	has_roll = false
+	local sorted_keys = get_sorted_keys(current_roll_tracker['passes'])
+	for _, key in ipairs(sorted_keys) do
+		new_text = new_text .. key .."\n"
+		raid_classes[key] = nil
+		has_roll = true
+	end
+	if not has_roll then new_text = new_text .."|cff666666None|r\n" end
+	
 
 
 	-- Show the remaining raid memebers
-	new_text = new_text .."No Action\n"
+	new_text = new_text .."\n|cffffff00No Action|r\n"
+	has_roll = false
+	local sorted_keys = get_sorted_keys(raid_classes)
+	for _, key in ipairs(sorted_keys) do
+		if raid_classes[key] ~= nil then
+			new_text = new_text .."|cff666666".. key .."|r\n"
+			has_roll = true
+		end
+	end
+	if not has_roll then new_text = new_text .."|cff666666None|r\n" end
 
-	print(new_text)
-	-- Dialog:Spawn("MSgtOSRollTracker", loot_table)
+	-- Finally set the current roll data to this new text
+	current_roll_text = new_text
+end
+
+local process_roll_request = function(addon_msg)
+	-- If there is not a current roll we don't need to do anything
+	if current_roll_tracker == nil then
+		return
+	end
+
+	-- only the master looters machine is alowed to generate a roll
+	if not master_looter then
+		return
+	end
+
+       	local my_roll = random(100)
+
+	roll_type = string.sub(addon_msg, 1, 6)
+	roll_user = string.sub(addon_msg, 7)
+
+	if roll_type == 'msroll' then
+		if current_roll_tracker['MS'][my_roll] == nil then
+			current_roll_tracker['MS'][my_roll] = {}
+		end
+		table.insert(current_roll_tracker['MS'][my_roll], roll_user)
+	elseif roll_type == 'osroll' then
+		if current_roll_tracker['OS'][my_roll] == nil then
+			current_roll_tracker['OS'][my_roll] = {}
+		end
+		table.insert(current_roll_tracker['OS'][my_roll], roll_user)
+	elseif roll_type == 'passes' then
+		current_roll_tracker['passes'][roll_user] = true
+	else
+		send_chat_message("Invalid roll request: ".. addon_msg)
+	end
+
+	generate_roll_text()
+       	C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "rollupdate ".. current_roll_text, "RAID");
+
+	-- Maybe we want to broadcast the roll? IDK, maybe a setting
+       	-- roll_message = player_name .." rolls ".. my_roll .." (1-100) ".. spec_type
+       	-- C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "rollupdate ".. player_name .." ".. my_roll .." ".. roll_type, "RAID");
+	--  DEFAULT_CHAT_FRAME:AddMessage(addon_msg, 1, 1, 0)
+end
+
+local loot_roll_frame = CreateFrame("Frame", "SwatterErrorFrame", UIParent)
+loot_roll_frame:Hide()
+loot_roll_frame:SetPoint("CENTER", "UIParent", "CENTER")
+loot_roll_frame:SetFrameStrata("TOOLTIP")
+loot_roll_frame:SetHeight(580)
+loot_roll_frame:SetWidth(300)
+loot_roll_frame:SetBackdrop({
+	bgFile = "Interface/Tooltips/ChatBubble-Background",
+	edgeFile = "Interface/Tooltips/ChatBubble-BackDrop",
+	tile = true, tileSize = 32, edgeSize = 32,
+	insets = { left = 32, right = 32, top = 32, bottom = 32 }
+})
+loot_roll_frame:SetBackdropColor(0,0,0, 1)
+--Swatter.Error:SetScript("OnShow", Swatter.ErrorShow)
+loot_roll_frame:SetMovable(true)
+loot_roll_frame:SetClampedToScreen(true)
+loot_roll_frame:SetToplevel(true)
+loot_roll_frame:SetScript("OnMouseDown", function() loot_roll_frame:StartMoving() end)
+loot_roll_frame:SetScript("OnMouseUp", function() loot_roll_frame:StopMovingOrSizing() end)
+
+-- local dragger = CreateFrame("Button", nil, loot_roll_frame)
+-- dragger:SetPoint("TOPLEFT", loot_roll_frame, "TOPLEFT", 10,-5)
+-- dragger:SetPoint("TOPRIGHT", loot_roll_frame, "TOPRIGHT", -10,-5)
+-- dragger:SetHeight(8)
+-- dragger:SetHighlightTexture("Interface\\FriendsFrame\\UI-FriendsFrame-HighlightBar")
+
+local close_button = CreateFrame("Button", "", loot_roll_frame, "OptionsButtonTemplate")
+close_button:SetText("Close")
+close_button:SetPoint("BOTTOMRIGHT", loot_roll_frame, "BOTTOMRIGHT", -10, 10)
+close_button:SetScript("OnClick", function() loot_roll_frame:Hide() end)
+close_button:SetFrameLevel(5)
+
+local next_button = CreateFrame("Button", "", loot_roll_frame, "OptionsButtonTemplate")
+next_button:SetText("Next >")
+next_button:SetPoint("BOTTOMRIGHT", close_button, "BOTTOMLEFT", -5, 0)
+next_button:SetScript("OnClick", Swatter.ErrorNext)
+next_button:SetFrameLevel(5)
+
+local prev_button = CreateFrame("Button", "", loot_roll_frame, "OptionsButtonTemplate")
+prev_button:SetText("< Prev")
+prev_button:SetPoint("BOTTOMRIGHT", next_button, "BOTTOMLEFT", -5, 0)
+prev_button:SetScript("OnClick", Swatter.ErrorPrev)
+prev_button:SetFrameLevel(5)
+
+local scroll_frame = CreateFrame("ScrollFrame", "", loot_roll_frame, "UIPanelScrollFrameTemplate")
+scroll_frame:SetPoint("TOPLEFT", loot_roll_frame, "TOPLEFT", 20, -20)
+scroll_frame:SetPoint("RIGHT", loot_roll_frame, "RIGHT", -30, 0)
+scroll_frame:SetPoint("BOTTOM", close_button, "TOP", 0, 10)
+
+local loot_info = CreateFrame("EditBox", "", scroll_frame)
+loot_info:SetWidth(450)
+loot_info:SetHeight(485)
+loot_info:SetMultiLine(true)
+loot_info:SetAutoFocus(false)
+loot_info:SetFontObject(GameFontHighlight)
+loot_info:SetScript("OnEditFocusGained", function() loot_info:ClearFocus() end )
+
+scroll_frame:SetScrollChild(loot_info)
+
+
+local update_roll_window = function(new_roll_data)
+	current_roll_text = new_roll_data
+	loot_info:SetText(new_roll_data)
+	loot_roll_frame:Show()
+end
+
+
+-- This openes up a roll for an item, everyone needs to run this so their client knows if a roll is opened
+local start_roll = function(item_link)
+	if current_roll_tracer ~= nil then
+		table.insert(legacy_rolls, current_roll_text)
+	end
+	current_roll_tracker = {
+		['item_link'] = item_link,
+		['passes'] = {},
+		['MS'] = {},
+		['OS'] = {},
+	}
+	roll_opened = true
+	-- Force the window to open for everyone
+	generate_roll_text()
+	update_roll_window(current_roll_text)
+end
+
+
+-- This ends a roll for an item, everyone needs to run this so their client knows if a roll ends
+local end_roll = function()
+	if current_roll_tracer ~= nil then
+		table.insert(legacy_rolls, current_roll_text)
+		if master_looter then
+			SendChatMessage("Roll for ".. current_roll_tracer['item_link'] .." has ended", "RAID")
+		end
+	end
+	current_roll_tracer = nil
+	roll_opened = false
+end
+
+SLASH_MSGTOSSTARTROLL1 = '/raidroll'
+SlashCmdList.MSGTOSSTARTROLL = function(msg, ...)
+	-- the raidroll command can only be be used by ML
+	if not master_looter then
+		return
+	end
+
+	if msg == nil then
+		send_chat_message("You need to specify an item to roll on")
+		return
+	end
+	if msg == 'end' then
+		if current_roll_tracker == nil then
+			send_chat_message("There is no roll currently opened")
+			return
+		end
+		SendChatMessage("Roll has ended for ".. current_roll_tracker['item_link'], "RAID")
+		C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "end", "RAID")
+	else
+		if current__roll_tracker ~= nil then
+			send_chat_message("You first need to end the existing roll for")
+			return
+		end
+		item_name, _, _, _, _, _, _, _, _, _, _ = GetItemInfo(msg)
+		if item_name == nil then
+			send_chat_message("Unable to lookup item ".. msg .." did you properly link the item?")
+			return
+		end
+
+		item_prio = 'None'
+		if addon_variables['prio_list'][item_name] ~= nil then
+			item_prio = addon_variables['prio_list'][item_name]
+		end
+		SendChatMessage("Roll for ".. item_link, "RAID_WARNING")
+		SendChatMessage("Prio for ".. item_link ..": ".. item_prio, "RAID")
+		C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "start ".. item_link, "RAID")
+	end
+end
+
+SLASH_MSGTOSOSROLL1 = '/osroll'
+SlashCmdList.MSGTOSOSROLL = function(msg, ...)
+	if roll_opened then
+		C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "osroll ".. player_name, "RAID");
+	else
+		print_no_roll_message()
+	end
+end
+
+SLASH_MSGTOSMSROLL1 = '/msroll'
+SlashCmdList.MSGTOSMSROLL = function(msg, ...)
+	if roll_opened then
+		C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "msroll ".. player_name, "RAID");
+	else
+		print_no_roll_message()
+	end
+end
+
+SLASH_MSGTOSPASSROLL1 = '/pass'
+SlashCmdList.MSGTOSPASSROLL = function(msg, ...)
+	if roll_opened then
+		C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", "passes ".. player_name, "RAID");
+	else
+		print_no_roll_message()
+	end
+end
+
+print_no_roll_message = function()
+	send_chat_message("There is not currently a roll opened")
 end
 
 local f = CreateFrame("Frame")
@@ -201,10 +355,16 @@ f:RegisterEvent("CHAT_MSG_ADDON")
 f:SetScript("OnEvent", function(self, event, ...)
 	if event == "CHAT_MSG_ADDON" then
 		message_type, addon_msg, level = ...
-                if message_type == 'MSgtOS_ROLL' then
-			process_incoming_roll(addon_msg)
-		elseif message_type == 'MSgtOS_ROLL_START' then
-			start_roll()
+		if message_type == 'MSgtOS_ROLL' then
+			if string.sub(addon_msg, 1, 6) == 'start ' then
+				start_roll(string.sub(addon_msg, 7))
+			elseif addon_msg == 'end' then
+				end_roll()
+			elseif string.sub(addon_msg, 1, 11) == 'rollupdate ' then
+				update_roll_window(string.sub(addon_msg, 12))
+			else
+				process_roll_request(addon_msg)
+			end
 		end
 	end
 end)
--- MSgtOS.lua
+++ MSgtOS.lua
@@ -1,3 +1,6 @@
+-- TODO:
+--	Can we auto-close copy window on ctl-c pressed?
+
 local MSgtOS, addon_variables = ...
 
 local rarity_names = { 'Poor', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legendary' }
@@ -6,7 +9,8 @@ local rarity_threashold = 4
 local threashold_exception = {
 	['Primal Hakkari Idol'] = true,
 	['Light Leather'] = true,
-	["Chunk of Boar Meat"] = true
+	["Chunk of Boar Meat"] = true,
+	['Dwarven Mild'] = true,
 } 
 local enable_logging = true
 local message_color = {
@@ -16,8 +20,8 @@ local message_color = {
 }
 local non_guild_pug_threashold_limit = 1
 local current_loot = {}
-local player_name = GetUnitName('player')
 local raid_group = nil
+local player_name = GetUnitName('player')
 
 local copy_dialog = Dialog:Register("MSgtOSCopyDialog", {
 	text = "Loot Copy\nUse your copy keyboard short cut to copy loot tbale to clipboard",
@@ -85,33 +89,33 @@ end
 local process_loot = function()
 	local unit_name = UnitName("target")
 	local loot_table = ''
-	if raid_group == nil then
-		-- Determine the Raid Group
-		raid_group = 'Ad-Hoc'
-		local day = date("%w")
-		if day == "6" then
-			raid_group = 'Weekend'
-		elseif day == "1" or day == "2" then
-			raid_group = 'Weekday'
-		end
-	end
+        if raid_group == nil then
+                -- Determine the Raid Group
+                raid_group = 'Ad-Hoc'
+                local day = date("%w")
+                if day == "6" then
+                        raid_group = 'Weekend'
+                elseif day == "1" or day == "2" then
+                        raid_group = 'Weekday'
+                end
+        end
 	for i = 1, GetNumLootItems() do
 		item_link = GetLootSlotLink(i);
 		if item_link ~= nil then
 			local item_name, _, item_rarity_number, _, _, _, _, _, _, _, _ = GetItemInfo(item_link)
 			local item_rarity = rarity_names[item_rarity_number + 1]
-			local zone = GetZoneText()
-			if zone == 'The Molten Core' then
-				zone = 'MC'
+                        local zone = GetZoneText()
+                        if zone == 'The Molten Core' then
+                                zone = 'MC'
 			elseif zone == 'Blackwing Lair' then
 				zone = 'BWL'
 			elseif zone == "Onyxia's Lair" then
 				zone = 'Ony'
-			elseif zone == "zul'gurub" then
+			elseif zone == "Zul'gurub" then
 				zone = 'ZG'
-			end
+                        end
 			if item_rarity_number >= rarity_threashold or threashold_exception[item_name] then
-				loot_table = loot_table .. date("%m-%d-%y") .. "\t".. zone .."\t".. raid_group .."\t\t".. item_name .."\t".. item_rarity .."\t0\t0\t".. unit_name .."\n"
+				loot_table = loot_table .. date("%m-%d-%y") .. "\t".. GetZoneText() .."\t".. raid_group .."\t\t".. item_name .."\t".. item_rarity .."\t0\t0\t".. unit_name .."\n"
 			end
 		end
 	end
@@ -120,64 +124,15 @@ local process_loot = function()
 	end
 end
 
-local process_raid_message = function(message)
-	if enable_logging then
-		for item_link in message:gmatch("|%x+|Hitem:.-|h.-|h|r") do
-			item_name, _, _, _, _, _, _, _, _, _, _ = GetItemInfo(item_link)
-			item_prio = 'None'
-			if addon_variables['prio_list'][item_name] ~= nil then
-				item_prio = addon_variables['prio_list'][item_name]
-			end
-			SendChatMessage("Prio for ".. item_link ..": ".. item_prio, "RAID")
-		end
-	end
-end
-
-
-local make_roll = function(main_spec)
-	local my_roll = random(100)
-	local spec_type = 'OS'
-	if main_spec then spec_type = 'MS' end
-	roll_message = player_name .." rolls ".. my_roll .." (1-100) ".. spec_type
-	C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", roll_message, "RAID");
-end
-
-SLASH_MSGTOSOSROLL1 = '/osroll'
-SlashCmdList.MSGTOSOSROLL = function(msg, editbox)
-	make_roll(false)
-end
-
-SLASH_MSGTOSMSROLL1 = '/msroll'
-SlashCmdList.MSGTOSMSROLL = function(msg, editbox)
-	make_roll(true)
-end
-
-SLASH_MSGTOSPASSROLL1 = '/pass'
-SlashCmdList.MSGTOSPASSROLL = function(msg, editbox)
-	C_ChatInfo.SendAddonMessage("MSgtOS_ROLL", player_name .." passes", "RAID");
-end
-
 local f = CreateFrame("Frame")
 f:RegisterEvent("LOOT_OPENED")
 f:RegisterEvent("RAID_ROSTER_UPDATE")
 f:RegisterEvent("GROUP_ROSTER_UPDATE")
-f:RegisterEvent("CHAT_MSG_RAID_WARNING");
-f:RegisterEvent("CHAT_MSG_ADDON")
 f:SetScript("OnEvent", function(self, event, ...)
 	if event == "LOOT_OPENED" and enable_logging then process_loot()
 	elseif event == "GROUP_ROSTER_UPDATE" or event == "RAID_ROSTER_UPDATE" then process_raid_change()
-	elseif event == "CHAT_MSG_RAID_WARNING" then
-		message = ...
-		process_raid_message(message)
-	elseif event == "CHAT_MSG_ADDON" then
-		message_type, addon_msg, level = ...
-		if message_type == 'MSgtOS_ROLL' then
-			DEFAULT_CHAT_FRAME:AddMessage(addon_msg, 1, 1, 0)
-		end
 	end
 end)
 
-
 -- In the case of a reload we want to process_raid_change
 process_raid_change()
-C_ChatInfo.RegisterAddonMessagePrefix("MSgtOS_ROLL")
